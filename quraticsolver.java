df4a2bb316fe95409f97c90f9be869a29ab92efd
Louis DePrez <ldeprez@umich.edu>
2024-03-07 14:29:34 -0500

    changing values for the jenkins part of the exam

--- quraticsolver.java
+++ quraticsolver.java
@@ -3,8 +3,8 @@ public class quraticsolver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 5;
-		int b = 6;
+		int a = 4;
+		int b = 7;
 		int c = 7;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
